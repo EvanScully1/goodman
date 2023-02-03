11673c4c73ebf38b030c8695740362f8c0b5b7a0
Evan Scully <98612784+ScullyAerialPhotography@users.noreply.github.com>
2023-02-03 13:54:26 -0500

    Add files via upload

---
+++ Goodman/goodman3.js
@@ -0,0 +1,10 @@
+<script>
+var elements = document.getElementsByClassName("column");
+var i;
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.flex = "50%"; 
+  }
+}
+
+</script>
